30868014795993c82e2062635bb3bfc57d840719
Purrtato <emmalinn.berglund@gmail.com>
2026-05-13 13:49:31 +0200

    Add initial routing setup with placeholder pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import RecipesPage from "./pages/RecipesPage"
+import RecipeDetailPage from "./pages/RecipeDetailPage"
+import FavoritesPage from "./pages/FavoritesPage"
 
-const App = () => {
-  
+function App() {
   return (
-    <div><h1>Hello Group 7!</h1></div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Hem</Link>
+        <Link to="/recipes">Recept</Link>
+        <Link to="/favorites">Favoriter</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipes" element={<RecipesPage />} />
+        <Route path="/recipes/:id" element={<RecipeDetailPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/RecipesPage.jsx
@@ -0,0 +1,7 @@
+export default function RecipesPage() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
