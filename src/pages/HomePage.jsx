5420f632df19a063bd57e3d41bd7bc63f28f45fe
Lynn Wigardt <lynn.wigardt@hotmail.com>
2026-05-25 12:37:06 +0200

    Add random meal feature to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react";
+import { getRandomMeal } from "../api/apiService";
+import MealCard from "../components/MealCard/MealCard";
+
+
 export default function HomePage() {
+  const [meal, setMeal] = useState(null);
+  const handleRandomMeal = async () => {
+  const randomMeal = await getRandomMeal();
+
+  console.log(randomMeal);
+
+  setMeal(randomMeal);
+};
+
+// Fetch a random meal when the page loads:
+  useEffect(() => {
+      handleRandomMeal();
+    }, []);
+
+
   return (
+
     <div>
-      <h1>Hello world</h1>
+
+     <h1>Vad ska vi äta idag?</h1>
+     <button onClick={handleRandomMeal}>
+      Slumpad måltid
+     </button>
+     {meal && <MealCard meal={meal} />}
+   
     </div>
-  )
+  
+  );
 }
