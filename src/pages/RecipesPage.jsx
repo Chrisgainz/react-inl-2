ad6f36452696cb1f4ebc0bf9df561ae59bc4ac82
Purrtato <emmalinn.berglund@gmail.com>
2026-05-25 16:36:44 +0200

    Add initial state setup for RecipesPage

--- src/pages/RecipesPage.jsx
+++ src/pages/RecipesPage.jsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from "react"
+import searchMealsbyName from "../services/searchMealsbyName"
+
 export default function RecipesPage() {
+  const [meals, setMeals] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [searchQuery, setSearchQuery] = useState("")
+
   return (
     <div>
       <h1>Hello world</h1>
